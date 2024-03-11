62e4b0b30d83df301c95b99ee75a2647c03c47d3
Jen Richardson <jen.richardson@autotrader.co.uk>
2024-03-11 16:25:13 +0000

    update serviceworker with repo name

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ async function enableMocking() {
     serviceWorker: {
       url:
         process.env.NODE_ENV !== 'development'
-          ? '/staybae-ui/mockServiceWorker.js'
+          ? '/stay-bae-man-digital/mockServiceWorker.js'
           : '/mockServiceWorker.js',
     },
     onUnhandledRequest: 'bypass',
